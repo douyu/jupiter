7c9d6ef21e16818733a50ad0606d7d24940e8cdb
OhBonsai <letbonsaibe@gmail.com>
2020-07-02 21:29:27 +0800

    refactor(redis): unified stud and cluster configuration

--- pkg/client/redis/redis_test.go
+++ pkg/client/redis/redis_test.go
@@ -21,18 +21,19 @@ import (
 func TestRedis(t *testing.T) {
 	// TODO(gorexlv): add redis ci
 	redisConfig := DefaultRedisConfig()
-	redisConfig.Addr = "localhost:6379"
-	redisClient := newRedisStub(&redisConfig)
+	redisConfig.Addrs = []string{"localhost:6379"}
+	redisConfig.Mode = StubMode
+	redisClient := redisConfig.Build()
 	err := redisClient.Client.Ping().Err()
 	if err != nil {
 		t.Errorf("redis ping failed:%v", err)
 	}
-	st := redisClient.Client.PoolStats()
+	st := redisClient.Stub().PoolStats()
 	t.Logf("running status %+v", st)
 	err = redisClient.Close()
 	if err != nil {
 		t.Errorf("redis close failed:%v", err)
 	}
-	st = redisClient.Client.PoolStats()
+	st = redisClient.Stub().PoolStats()
 	t.Logf("close status %+v", st)
 }
