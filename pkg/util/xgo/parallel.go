49ed9a026e82b454026b1b6359853df8e03a0210
mlboy <ml3@meitu.com>
2020-07-08 11:52:29 +0800

    pref: optimize RestrictParallelWithErrorChan method

--- pkg/util/xgo/parallel.go
+++ pkg/util/xgo/parallel.go
@@ -60,38 +60,41 @@ func ParallelWithErrorChan(fns ...func() error) chan error {
 // returns a chan of errors.
 func RestrictParallelWithErrorChan(concurrency int, fns ...func() error) chan error {
 	total := len(fns)
-
 	if concurrency <= 0 {
 		concurrency = 1
 	}
-
 	if concurrency > total {
 		concurrency = total
 	}
-
 	var wg sync.WaitGroup
-	wg.Add(total)
-
 	errs := make(chan error, total)
-	sem := make(chan struct{}, concurrency)
-	for i := 0; i < cap(sem); i++ {
-		sem <- struct{}{}
-	}
+	jobs := make(chan func() error, concurrency)
+	wg.Add(concurrency)
+	for i := 0; i < concurrency; i++ {
+		//consumer
+		go func(jobs chan func() error, errs chan error) {
+			defer wg.Done()
+			for {
+				select {
+				case fn, ok := <-jobs:
+					if !ok {
+						return
+					}
+					errs <- try(fn, nil)
+				}
 
-	go func(sem chan struct{}, errs chan error) {
+			}
+		}(jobs, errs)
+	}
+	go func(errs chan error) {
+		//producer
+		for _, fn := range fns {
+			jobs <- fn
+		}
+		close(jobs)
+		//wait for block errs
 		wg.Wait()
 		close(errs)
-		close(sem)
-	}(sem, errs)
-
-	for _, fn := range fns {
-		go func(fn func() error, sem chan struct{}, errs chan error) {
-			defer wg.Done()
-			<-sem
-			errs <- try(fn, nil)
-			sem <- struct{}{}
-		}(fn, sem, errs)
-	}
-
+	}(errs)
 	return errs
 }
