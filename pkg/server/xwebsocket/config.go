1eac0261c5a1d3f087277576ec929ab59bfcea04
mlboy <mlboy@126.com>
2020-07-13 14:19:27 +0800

    wip

---
+++ pkg/server/xwebsocket/config.go
@@ -0,0 +1,138 @@
+// Copyright 2020 Douyu
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package xgrpc
+
+import (
+	"fmt"
+
+	"github.com/douyu/jupiter/pkg/ecode"
+	"github.com/douyu/jupiter/pkg/xlog"
+
+	"github.com/douyu/jupiter/pkg/conf"
+	"google.golang.org/grpc"
+)
+
+// Config ...
+type Config struct {
+	Host string
+	Port int
+	// Network network type, tcp4 by default
+	Network string `json:"network" toml:"network"`
+	// DisableTrace disbale Trace Interceptor, false by default
+	DisableTrace bool
+	// DisableMetric disable Metric Interceptor, false by default
+	DisableMetric bool
+	// SlowQueryThresholdInMilli, request will be colored if cost over this threshold value
+	SlowQueryThresholdInMilli int64
+	serverOptions             []grpc.ServerOption
+	streamInterceptors        []grpc.StreamServerInterceptor
+	unaryInterceptors         []grpc.UnaryServerInterceptor
+
+	logger *xlog.Logger
+}
+
+// StdConfig represents Standard gRPC Server config
+// which will parse config by conf package,
+// panic if no config key found in conf
+func StdConfig(name string) *Config {
+	return RawConfig("jupiter.server." + name)
+}
+
+// RawConfig ...
+func RawConfig(key string) *Config {
+	var config = DefaultConfig()
+	if err := conf.UnmarshalKey(key, &config); err != nil {
+		config.logger.Panic("grpc server parse config panic",
+			xlog.FieldErrKind(ecode.ErrKindUnmarshalConfigErr),
+			xlog.FieldErr(err), xlog.FieldKey(key),
+			xlog.FieldValueAny(config),
+		)
+	}
+	return config
+}
+
+// DefaultConfig represents default config
+// User should construct config base on DefaultConfig
+func DefaultConfig() *Config {
+	return &Config{
+		Network:                   "tcp4",
+		Host:                      "127.0.0.1",
+		Port:                      9092,
+		DisableMetric:             false,
+		DisableTrace:              false,
+		SlowQueryThresholdInMilli: 500,
+		logger:                    xlog.JupiterLogger.With(xlog.FieldMod("server.grpc")),
+		serverOptions:             []grpc.ServerOption{},
+		streamInterceptors:        []grpc.StreamServerInterceptor{},
+		unaryInterceptors:         []grpc.UnaryServerInterceptor{},
+	}
+}
+
+// WithServerOption inject server option to grpc server
+// User should not inject interceptor option, which is recommend by WithStreamInterceptor
+// and WithUnaryInterceptor
+func (config *Config) WithServerOption(options ...grpc.ServerOption) *Config {
+	if config.serverOptions == nil {
+		config.serverOptions = make([]grpc.ServerOption, 0)
+	}
+	config.serverOptions = append(config.serverOptions, options...)
+	return config
+}
+
+// WithStreamInterceptor inject stream interceptors to server option
+func (config *Config) WithStreamInterceptor(intes ...grpc.StreamServerInterceptor) *Config {
+	if config.streamInterceptors == nil {
+		config.streamInterceptors = make([]grpc.StreamServerInterceptor, 0)
+	}
+
+	config.streamInterceptors = append(config.streamInterceptors, intes...)
+	return config
+}
+
+// WithUnaryInterceptor inject unary interceptors to server option
+func (config *Config) WithUnaryInterceptor(intes ...grpc.UnaryServerInterceptor) *Config {
+	if config.unaryInterceptors == nil {
+		config.unaryInterceptors = make([]grpc.UnaryServerInterceptor, 0)
+	}
+
+	config.unaryInterceptors = append(config.unaryInterceptors, intes...)
+	return config
+}
+
+// Build ...
+func (config *Config) Build() *Server {
+	if !config.DisableTrace {
+		config.unaryInterceptors = append(config.unaryInterceptors, traceUnaryServerInterceptor)
+		config.streamInterceptors = append(config.streamInterceptors, traceStreamServerInterceptor)
+	}
+
+	if !config.DisableMetric {
+		config.unaryInterceptors = append(config.unaryInterceptors, prometheusUnaryServerInterceptor)
+		config.streamInterceptors = append(config.streamInterceptors, prometheusStreamServerInterceptor)
+	}
+
+	return newServer(config)
+}
+
+// WithLogger ...
+func (config *Config) WithLogger(logger *xlog.Logger) *Config {
+	config.logger = logger
+	return config
+}
+
+// Address ...
+func (config Config) Address() string {
+	return fmt.Sprintf("%s:%d", config.Host, config.Port)
+}
---
+++ pkg/server/xwebsocket/server.go
@@ -0,0 +1,82 @@
+// Copyright 2020 Douyu
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package xgrpc
+
+import (
+	"context"
+
+	"github.com/douyu/jupiter/pkg"
+	"github.com/gorilla/websocket"
+
+	"github.com/douyu/jupiter/pkg/server"
+	"google.golang.org/grpc"
+)
+
+// Server ...
+type Server struct {
+	Upgrader *websocket.Upgrader
+	config   *Config
+}
+
+func newServer(config *Config) *Server {
+	upgrader := &websocket.Upgrader{}
+	// listener, err := net.Listen(config.Network, config.Address())
+	// if err != nil {
+	// 	config.logger.Panic("new grpc server err", xlog.FieldErrKind(ecode.ErrKindListenErr), xlog.FieldErr(err))
+	// }
+	// config.Port = listener.Addr().(*net.TCPAddr).Port
+	return &Server{Upgrader: upgrader, config: config}
+}
+
+// Serve implements server.Server interface.
+func (s *Server) Serve() error {
+	err := s.Server.Serve(s.listener)
+	if err == grpc.ErrServerStopped {
+		return nil
+	}
+	return err
+}
+
+// Stop implements server.Server interface
+// it will terminate echo server immediately
+func (s *Server) Stop() error {
+	s.Server.Stop()
+	return nil
+}
+
+// GracefulStop implements server.Server interface
+// it will stop echo server gracefully
+func (s *Server) GracefulStop(ctx context.Context) error {
+	s.Server.GracefulStop()
+	return nil
+}
+
+// Info returns server info, used by governor and consumer balancer
+// TODO(gorexlv): implements government protocol with juno
+func (s *Server) Info() *server.ServiceInfo {
+	return &server.ServiceInfo{
+		Name:      pkg.Name(),
+		Scheme:    "grpc",
+		IP:        s.Host,
+		Port:      s.Port,
+		Weight:    0.0,
+		Enable:    true,
+		Healthy:   true,
+		Metadata:  map[string]string{},
+		Region:    "",
+		Zone:      "",
+		GroupName: "",
+	}
+}
