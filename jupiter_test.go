1ebdbc4784849f9539a96d0f187355c040eb3356
mlboy <mlboy@126.com>
2020-08-05 03:25:02 +0800

    add test cover

--- jupiter_test.go
+++ jupiter_test.go
@@ -17,6 +17,7 @@ package jupiter
 import (
 	"context"
 	"errors"
+	"fmt"
 	"testing"
 	"time"
 
@@ -53,13 +54,30 @@ func (s *testServer) GracefulStop(ctx context.Context) error {
 func (s *testServer) Info() *server.ServiceInfo {
 	return &server.ServiceInfo{}
 }
-func TestApplication_New(t *testing.T) {
-	Convey("test application run serve", t, func(c C) {
+
+var errTest = fmt.Errorf("test error")
+
+func Test_Unit_Application_New(t *testing.T) {
+	t.Run("no params", func(t *testing.T) {
 		_, err := New()
-		So(err, ShouldBeNil)
+		assert.Nil(t, err)
+	})
+	t.Run("without error", func(t *testing.T) {
+		fn := func() error {
+			return nil
+		}
+		_, err := New(fn)
+		assert.Nil(t, err)
+	})
+	t.Run("with error", func(t *testing.T) {
+		fn := func() error {
+			return errTest
+		}
+		_, err := New(fn)
+		assert.Equal(t, errTest, err)
 	})
 }
-func TestApplication_Run_1(t *testing.T) {
+func TestApplication_Run(t *testing.T) {
 	Convey("test application run serve", t, func(c C) {
 		srv := &testServer{
 			ServeErr: errors.New("when server call serve error"),
@@ -296,10 +314,35 @@ func Test_Unit_Application_Job(t *testing.T) {
 }
 
 func Test_Unit_Application_startJobs(t *testing.T) {
-	app := &Application{}
-	app.initialize()
-	err := app.startJobs()
-	assert.Nil(t, err, err)
+	t.Run("without jobs", func(t *testing.T) {
+		app := &Application{}
+		app.initialize()
+		err := app.startJobs()
+		assert.Nil(t, err, err)
+	})
+	t.Run("with a jobs", func(t *testing.T) {
+		app := &Application{}
+		app.initialize()
+		app.jobs["test"] = &namedJobRunner{}
+		err := app.startJobs()
+		assert.Nil(t, err, err)
+	})
+}
+
+func Test_Unit_Application_startWorkers(t *testing.T) {
+	t.Run("without workers", func(t *testing.T) {
+		app := &Application{}
+		app.initialize()
+		err := app.startWorkers()
+		assert.Nil(t, err, err)
+	})
+	t.Run("with a workers", func(t *testing.T) {
+		app := &Application{}
+		app.initialize()
+		app.workers = append(app.workers, &testWorker{})
+		err := app.startWorkers()
+		assert.Nil(t, err, err)
+	})
 }
 
 /*
