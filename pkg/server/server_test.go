679f55c697c2545de948d99fe569f2e2ec38d0b8
=myoasm <=1272644166@qq.com>
2022-04-01 19:16:15 +0800

    fix:Reproduce the pannic problem of  issue  #293

--- pkg/server/server_test.go
+++ pkg/server/server_test.go
@@ -15,6 +15,7 @@
 package server
 
 import (
+	"fmt"
 	"github.com/douyu/jupiter/pkg/constant"
 	"google.golang.org/grpc/attributes"
 	"google.golang.org/grpc/resolver"
@@ -75,6 +76,75 @@ func Test_ServiceInfo(t *testing.T) {
 
 	if address3.Equal(address1) {
 		t.Fatalf("%+v.Equals(%+v) = true; want false", address3, address1)
+	}
+
+}
+
+// Reproduce the pannic problem of  issue  #293
+// The structure #ServiceInfo# inside the test case
+// does not implement the equal method, so the
+// comparison at runtime results in panic
+func TestNotImplementEqual(t *testing.T) {
+
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r)
+		}
+	}()
+	// The previous structure:  Equal method is not implemented
+	type ServiceInfo struct {
+		Name     string               `json:"name"`
+		AppID    string               `json:"appId"`
+		Scheme   string               `json:"scheme"`
+		Address  string               `json:"address"`
+		Weight   float64              `json:"weight"`
+		Enable   bool                 `json:"enable"`
+		Healthy  bool                 `json:"healthy"`
+		Metadata map[string]string    `json:"metadata"`
+		Region   string               `json:"region"`
+		Zone     string               `json:"zone"`
+		Kind     constant.ServiceKind `json:"kind"`
+		// Deployment 部署组: 不同组的流量隔离
+		// 比如某些服务给内部调用和第三方调用，可以配置不同的deployment,进行流量隔离
+		Deployment string `json:"deployment"`
+		// Group 流量组: 流量在Group之间进行负载均衡
+		Group    string              `json:"group"`
+		Services map[string]*Service `json:"services" toml:"services"`
+	}
+
+
+	info1 := ServiceInfo{
+		Name:    "main",
+		Address: "127.0.0.1:1234",
+		Weight:  100,
+		Kind:    constant.ServiceProvider,
+		Group:   "g1",
+	}
+
+	info2 := ServiceInfo{
+		Name:    "main",
+		Address: "127.0.0.1:1234",
+		Weight:  100,
+		Kind:    constant.ServiceProvider,
+		Group:   "g1",
+	}
+
+
+	var (
+		address1, address2 resolver.Address
+	)
+
+	// Attributes as above
+	address1.Addr = info1.Address
+	address1.Attributes = attributes.New(constant.KeyServiceInfo, info1)
+
+	address2.Addr = info2.Address
+	address2.Attributes = attributes.New(constant.KeyServiceInfo, info2)
+
+
+	// This will cause panic
+	if !address1.Equal(address2) {
+		t.Fatalf("%+v.Equals(%+v) = false; want true", address1, address2)
 	}
 
 }
