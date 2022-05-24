f1ee84458654873c9e937f66b0b6446b8e92ed20
longjunhu <hulongjun@douyu.tv>
2022-05-24 20:27:14 +0800

    update resty test

--- pkg/client/resty/resty_test.go
+++ pkg/client/resty/resty_test.go
@@ -15,13 +15,13 @@
 package resty
 
 import (
-	"fmt"
-	"github.com/douyu/jupiter/pkg/xtrace"
-	"github.com/douyu/jupiter/pkg/xtrace/jaeger"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	"errors"
+	"net/url"
 	"testing"
 	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
 func TestE2ESuites(t *testing.T) {
@@ -32,42 +32,38 @@ func TestE2ESuites(t *testing.T) {
 var _ = Describe("normal case", func() {
 	It("httptest", func() {
 		config := DefaultConfig()
-		config.Addr = "http://127.0.0.1:9099"
+		config.Addr = "https://httpbin.org"
 		config.EnableTrace = true
-
-		jaegerConfig := jaeger.DefaultConfig()
-		xtrace.SetGlobalTracer(jaegerConfig.Build())
-		//config.MustBuild().R().SetContext().Get()
-		res, err := config.MustBuild().R().Get("/hello")
-		fmt.Println("============》err", err)
-		fmt.Println("============》err", res)
+		res, err := config.MustBuild().R().Get("/get")
 		Expect(err).Should(BeNil())
-		time.Sleep(10 * time.Second)
-		//Expect(res.Status()).Should(Equal("200 OK"))
+		Expect(res.Status()).Should(Equal("200 OK"))
 	})
 
-	/*It("slowlog", func() {
+	It("slowlog", func() {
 		config := DefaultConfig()
-		config.Addr = "http://127.0.0.1:9099"
+		config.Addr = "https://httpbin.org"
 		// 测试慢日志
 		config.SlowThreshold = time.Millisecond
-		_, err := config.MustBuild().R().Get("/hello")
+		res, err := config.MustBuild().R().Get("/get")
 
 		Expect(err).Should(BeNil())
-		//Expect(res.Status()).Should(Equal("200 OK"))
+		Expect(res.Status()).Should(Equal("200 OK"))
 	})
 
 	It("on error", func() {
-		//config := DefaultConfig()
-		//config.Addr = "http://127.0.0.1:9099"
-		//jaegerConfig := jaeger.DefaultConfig()
-		//// fast flush trace
-		//
-		//xtrace.SetGlobalTracer(jaegerConfig.Build())
-		//
-		//_, err := config.MustBuild().R().Get("/hello")
-		//Expect(err.(*url.Error).Err).Should(BeEquivalentTo(errors.New("auto redirect is disabled")))
-		////Expect(res.Status()).Should(Equal("302 Found"))
-		//time.Sleep(100 * time.Millisecond)
-	})*/
+		config := DefaultConfig()
+		config.Addr = "https://httpbin.org"
+
+		res, err := config.MustBuild().R().Get("/status/302")
+		Expect(err.(*url.Error).Err).Should(BeEquivalentTo(errors.New("auto redirect is disabled")))
+		Expect(res.Status()).Should(Equal("302 Found"))
+		time.Sleep(100 * time.Millisecond)
+	})
+
+	It("no addr", func() {
+		Expect(func() {
+			config := DefaultConfig()
+			config.MustBuild().R().Get("")
+		}).Should(Panic())
+	})
 })
