914d96e67916bc79ecfeaee6da62157c733cb6d0
大可 <hnlq.sysu@gmail.com>
2022-08-25 18:41:34 +0800

    fix

--- pkg/store/gorm/instance_test.go
+++ pkg/store/gorm/instance_test.go
@@ -20,24 +20,6 @@ func (u User) TableName() string {
 	return "users"
 }
 
-func TestClient_New(t *testing.T) {
-	// 1. 从配置文件中创建client
-	_ = StdNew("demo") // 读取jupiter.mysql.demo配置，创建实例
-
-	// 2. 从配置文件按照自定义格式创建client
-	_ = dial("demo", RawConfig("jupiter.pq.demo"))
-
-	// 3. 自定义配置，创建client
-	_ = dial("demo", &Config{
-		Debug:           false,
-		MaxIdleConns:    0,
-		MaxOpenConns:    0,
-		ConnMaxLifetime: 0,
-		OnDialError:     "",
-		raw:             nil,
-	})
-}
-
 func TestStdNew(t *testing.T) {
 	type args struct {
 		name string
