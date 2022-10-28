87751ba89ed624583cf7be14fd5f0d47fa3202e9
大可 <hnlq.sysu@gmail.com>
2022-10-28 11:25:39 +0800

    fix it

--- website/docs/.vuepress/config.js
+++ website/docs/.vuepress/config.js
@@ -60,7 +60,7 @@ module.exports = {
       },
     ],
     // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    repo: "douyu/jupiter-website",
+    repo: "douyu/jupiter",
     // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
     // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
     repoLabel: "查看文档源码",
