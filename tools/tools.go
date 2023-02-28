0ebb2911ab74ebdc4b2f02b880c23b9f2cfe0f19
chory <chory@douyu.tv>
2023-02-28 19:31:55 +0800

    暂时不导入插件

--- tools/tools.go
+++ tools/tools.go
@@ -9,7 +9,6 @@ import (
 	_ "github.com/douyu/jupiter/cmd/protoc-gen-go-echo"
 	_ "github.com/douyu/jupiter/cmd/protoc-gen-go-gin"
 	_ "github.com/douyu/jupiter/cmd/protoc-gen-go-xerror"
-	_ "github.com/douyu/jupiter/cmd/protoc-gen-go-xfieldmask"
 	_ "github.com/envoyproxy/protoc-gen-validate"
 	_ "github.com/go-swagger/go-swagger/cmd/swagger"
 	_ "github.com/google/wire/cmd/wire"
