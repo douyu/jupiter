010e76abb0700cd98875b0e7801b6695a221bdc5
彭业昌 <refrain@douyu.tv>
2023-05-09 10:25:42 +0800

    feat(freecache): 删除无用方法

--- pkg/cache/xfreecache/v2/encode_bench_test.go
+++ pkg/cache/xfreecache/v2/encode_bench_test.go
@@ -2,6 +2,8 @@ package xfreecache
 
 import (
 	"encoding/json"
+	"reflect"
+	"sync"
 	"testing"
 
 	helloworldv1 "github.com/douyu/jupiter/proto/helloworld/v1"
@@ -91,4 +93,40 @@ func BenchmarkDecodeProtoWithReflectAndPool(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_, _ = unmarshalWithPool[*helloworldv1.SayHiResponse](res, pool)
 	}
+}
+
+var pools sync.Map
+
+func getPool[T any]() *sync.Pool {
+	var value T
+	if msg, ok := any(value).(proto.Message); ok {
+		msgType := reflect.TypeOf(msg).Elem()
+		if pool, ok2 := pools.Load(msgType.String()); ok2 {
+			return pool.(*sync.Pool)
+		}
+
+		pool := &sync.Pool{
+			New: func() any {
+				// Make a new one, and throw it back into T
+				msgN := reflect.New(msgType).Interface().(proto.Message)
+				return msgN
+			},
+		}
+		pools.Store(msgType.String(), pool)
+		return pool
+	}
+	return nil
+}
+
+// 反序列化，如果是pb格式，则使用proto序列化 使用sync.Pool-存在并发问题
+func unmarshalWithPool[T any](body []byte, pool *sync.Pool) (value T, err error) {
+	if _, ok := any(value).(proto.Message); ok { // Constrained to proto.Message
+		msg := pool.Get().(proto.Message)
+		err = proto.Unmarshal(body, msg)
+		value = msg.(T)
+		pool.Put(msg)
+	} else {
+		err = json.Unmarshal(body, &value)
+	}
+	return
 }
