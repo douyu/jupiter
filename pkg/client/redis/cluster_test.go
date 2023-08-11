574f3ffbb256680ab6dee40ddb993e07e2d0a9b8
liwc1 <liwc1@jiguang.cn>
2023-08-11 16:25:38 +0800

    修改cluster 单测

--- pkg/client/redis/cluster_test.go
+++ pkg/client/redis/cluster_test.go
@@ -12,7 +12,7 @@ func Test_Cluster(t *testing.T) {
 		var client *ClusterClient
 		defer func() {
 			if r := recover(); r != nil {
-				assert.Equal(t, "no cluster for default", r.(string))
+				assert.Equal(t, "cluster redis addr is empty", r.(string))
 				assert.Nil(t, client)
 			}
 		}()
@@ -29,13 +29,16 @@ func Test_Cluster(t *testing.T) {
 	})
 	t.Run("normal start", func(t *testing.T) {
 		config.Addr = []string{"127.0.0.1:7000", "127.0.0.1:7001", "127.0.0.1:7002"}
+		config.name = "test"
 		client, err := config.BuildCluster()
-		assert.Nil(t, err)
-		assert.NotNil(t, client)
-		err = client.Ping(context.Background()).Err()
 		if err != nil {
-			t.Errorf("Test_Cluster ping err %v", err)
+			t.Fatalf("Failed to build cluster: %v", err)
 		}
+		if client == nil {
+			t.Fatal("Client is nil")
+		}
+		err = client.Ping(context.Background()).Err()
+		assert.Nil(t, err)
 	})
 
 }
